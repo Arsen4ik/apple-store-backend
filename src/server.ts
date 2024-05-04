c926eaae66beb4da964f029fb88fa5ad6bb66d3a
Arsen4ik <arsenfazleev@gmail.com>
2024-05-05 02:13:25 +0300

    commit 2.1: db updated

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-app.use('/api', router)
+app.use('/', router)
 
 // app.get('/', async (req, res) => {
 //     const users = await prisma.users.findMany()
