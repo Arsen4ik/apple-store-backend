50b73c52affb3feff2d98e34db7c60087d107468
Arsen4ik <arsenfazleev@gmail.com>
2024-05-05 11:38:28 +0300

    commit 5: tested

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -15,12 +15,9 @@ import db from './db'
 // }) => {
 const createGadget = async (data: Gadget) => {
     try {
-        console.log(data);
         const newGadget = await db.gadget.create({
             data
         })
-        console.log(newGadget);
-
         return newGadget
     } catch (e) {
         console.error('create gadget error: ', e);
@@ -102,10 +99,7 @@ export const createComputer = async (data: Computer) => {
 export const createHeadphones = async (data: Headphones) => {
     const { headphones, gadget } = data
     try {
-        console.log(gadget);
         const newGadget = await createGadget(gadget);
-        // console.log(data, newGadget);
-
         const newHeadphones = await db.headphones.create({
             data: {
                 ...headphones,
